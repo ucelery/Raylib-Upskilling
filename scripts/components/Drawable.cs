e6068216a0ae8f7e69580999079b6792eb1a0a65
ucelery <atondavid.ad@gmail.com>
2025-10-03 22:43:23 +0800

    Make Texture Public

--- scripts/components/Drawable.cs
+++ scripts/components/Drawable.cs
@@ -4,8 +4,9 @@ using Raylib_cs;
 public class Drawable : Component
 {
     private string texturePath;
-    private Texture2D texture;
     private TextureFilter filter = TextureFilter.Bilinear;
+
+    public Texture2D Texture { get; private set; }
 
     public Drawable(string texturePath)
     {
@@ -20,18 +21,18 @@ public class Drawable : Component
 
     public override void Initialize()
     {
-        texture = Raylib.LoadTexture(this.texturePath);
-        Raylib.SetTextureFilter(texture, this.filter);
+        Texture = Raylib.LoadTexture(this.texturePath);
+        Raylib.SetTextureFilter(Texture, this.filter);
     }
 
     public override void Update()
     {
-        Vector2 offset = new Vector2(texture.Width / 2, texture.Height / 2);
-        Raylib.DrawTextureV(texture, GameObject.position - offset, Color.White);
+        Vector2 offset = new Vector2(Texture.Width / 2, Texture.Height / 2);
+        Raylib.DrawTextureV(Texture, GameObject.position - offset, Color.White);
     }
 
     public override void Unload()
     {
-        Raylib.UnloadTexture(texture);
+        Raylib.UnloadTexture(Texture);
     }
 }
