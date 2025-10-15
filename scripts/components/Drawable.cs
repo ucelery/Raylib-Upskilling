a66fb0b714790bc31b970a6c71c0675f6c189c9e
ucelery <atondavid.ad@gmail.com>
2025-10-15 23:11:37 +0800

    Implement Texture Flipping

--- scripts/components/Drawable.cs
+++ scripts/components/Drawable.cs
@@ -4,8 +4,9 @@ using Raylib_cs;
 public class Drawable : Component
 {
     private TextureFilter filter = TextureFilter.Bilinear;
-    private float scale = 1;
+    public float scale = 1;
     private float rotation = 0;
+    private Vector2 flip = Vector2.One;
 
     public Texture2D Texture { get; private set; }
 
@@ -31,14 +32,29 @@ public class Drawable : Component
 
     public override void Update()
     {
-        Vector2 offset = new Vector2(Texture.Width / 2, Texture.Height / 2);
-        Raylib.DrawTextureV(Texture, GameObject.position - offset, Color.White);
-        Raylib.DrawTextureEx(Texture, GameObject.position - offset, 0, scale, Color.White);
+        Vector2 offset = new Vector2(Texture.Width / 2 * scale, Texture.Height / 2 * scale);
+        // Raylib.DrawTextureEx(Texture, GameObject.position - offset, 0, scale, Color.White);
+        Rectangle source = new Rectangle(0, 0, Texture.Width * flip.X, Texture.Height * flip.Y);
+        Rectangle dest = new Rectangle(GameObject.position - offset, Texture.Width * scale, Texture.Height * scale);
+        
+        Raylib.DrawTexturePro(
+            Texture,
+            source,
+            dest,
+            new Vector2(0, 0),
+            0f,               
+            Color.White
+        );
     }
 
     public void SetScale(float scale)
     {
         this.scale = scale;
+    }
+
+    public void SetFlip(Vector2 flip)
+    {
+        this.flip = flip;
     }
 
     public void SetRotation(float rotation)
